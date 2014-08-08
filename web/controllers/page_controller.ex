b11e7c5c387a7ba1f9f80249423e88a36c341f96
Josh Adams <josh@isotope11.com>
2014-08-07 19:28:03 -0500

    Remove unnecessary Repo alias

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,6 @@
 defmodule BeamToolboxWeb.PageController do
   use Phoenix.Controller
   alias BeamToolboxData.Models.Project
-  alias BeamToolboxData.Repo
 
   def index(conn, _params) do
     projects = Project.for_category(:uncategorized)
