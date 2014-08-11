14516e44462246b37e40c1d65a2b8e18c85cbb99
Josh Adams <josh@isotope11.com>
2014-08-11 00:29:57 -0500

    Add source link, description

--- web/views/project_view.ex
+++ web/views/project_view.ex
@@ -1,7 +1,19 @@
 defmodule BeamToolboxWeb.ProjectView do
   use BeamToolboxWeb.Views
+  alias BeamToolboxData.Models.Project
 
   def hex_package_link(project) do
     "<a href='https://hex.pm/packages/#{project.key}'>View this project on hex</a>"
+  end
+
+  def source_link(project) do
+    link_if_exists(Project.source_link(project), "Source")
+  end
+
+  def link_if_exists(link, text) do
+    case link do
+      nil -> ""
+      _ -> "<a href='#{link}'>#{text}</a>"
+    end
   end
 end
