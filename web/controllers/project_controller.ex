231c0e6d86ab1bd8fe6440186a687ae55e81c254
Josh Adams <josh@isotope11.com>
2014-08-13 23:27:07 -0500

    Add 'other projects in this project's category' sidebar

--- web/controllers/project_controller.ex
+++ web/controllers/project_controller.ex
@@ -4,6 +4,12 @@ defmodule BeamToolboxWeb.ProjectController do
 
   def show(conn, %{"id" => key}) do
     project = Project.find_by_key(key)
-    render conn, "show", %{project: project}
+    category_projects = other_projects_for_category(project)
+    render conn, "show", %{project: project, category_projects: category_projects}
+  end
+
+  defp other_projects_for_category(project) do
+    Project.for_category(Project.category(project))
+    |> Enum.filter(fn(other) -> other != project end)
   end
 end
