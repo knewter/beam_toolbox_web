a44ecb46d14e115e949a07946616075bc33a9685
Josh Adams <josh@isotope11.com>
2014-08-13 20:56:49 -0500

    Some more stylistic updates...

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -2,9 +2,11 @@ defmodule BeamToolboxWeb.PageView do
   use BeamToolboxWeb.Views
 
   def category_link :uncategorized do
-    render "category.html", %{category: %Category{slug: "uncategorized", name: "Uncategorized"}}
+    projects_count = Project.for_category(:uncategorized) |> Enum.count
+    render "category.html", %{category: %Category{slug: "uncategorized", name: "Uncategorized"}, count: projects_count}
   end
   def category_link category do
-    render "category.html", %{category: category}
+    projects_count = Project.for_category(category) |> Enum.count
+    render "category.html", %{category: category, count: projects_count}
   end
 end
